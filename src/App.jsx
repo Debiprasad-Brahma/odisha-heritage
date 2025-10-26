5216292f9dd6145f27106c54939bd26c1692c483
Debiprasad-Brahma <brahmadebiprasad@gmail.com>
2025-10-26 10:35:36 +0530

    add the login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Gallery from "./pages/Gallery";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
 import Login from "./pages/Login";
-
+import Register from "./pages/Register";
 const App = () => {
   return (
     <div className="min-h-screen bg-[#FFF8F0]">
@@ -23,6 +23,7 @@ const App = () => {
           <Route path="/blog" element={<Blog />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </div>
