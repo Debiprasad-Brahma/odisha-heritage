487effd3399afbfc43d0cb361aa15c99d03b81ae
Debiprasad-Brahma <brahmadebiprasad@gmail.com>
2025-12-07 19:23:57 +0530

    add the password Hashing and Checking

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -4,41 +4,60 @@ import jwt from "jsonwebtoken"
 import crypto from "crypto"
 import {type} from "os"
 
-
 //* The User Schema
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
+  {
+    timestamps: true,
   },
-  isEmailVerified: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  refreshToken: {
-    type: String,
-  },
-  forgotPasswordToken: {
-    type: String,
-  },
-  forgotPasswordExpiry: {
-    type: Date,
-  },
-  emailVerificationToken: {
-    type: String,
-  },
-  emailVerificationExpiry: {
-    type: Date,
-  },
+)
+
+//* What you need to do before saving into database - hashPassword (pre function)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
 })
+
+//* Method for checking the password is correct or not during user login - compare
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema)
